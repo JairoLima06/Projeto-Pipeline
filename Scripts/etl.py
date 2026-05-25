356bcbba07eb0f880c8d1e351c9663a2c1c28854
JairoLima06 <francisco.jairodiasdelima@gmail.com>
2026-05-25 16:54:06 -0300

    Estrutura Inicial

---
+++ Scripts/etl.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+# Serve para Ler o arquivo CSV
+df = pd.read_csv("data/sales_data.csv")
+
+# Serve para mostrar as primeiras linhas
+print(df.head())
+
+##########################################
+
+# Remover linhas vazias
+df.dropna(inplace=True)
+
+# Remover duplicados
+df.drop_duplicates(inplace=True)
+
+print(df.info())
+
+#Conexão com o MySQL
+
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    "mysql+pymysql://root:Jairo.20@localhost/pipeline_vendas"
+)
+
+#Enviar dados para o banco
+
+df.to_sql(
+    "vendas",
+    engine,
+    if_exists="replace",
+    index=False
+)
